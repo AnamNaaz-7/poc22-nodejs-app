2d1b9a1d43a29eb1621fd90361899bbac11044e6
AnamNaaz-7 <115145295+AnamNaaz-7@users.noreply.github.com>
2026-05-27 12:16:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js CI/CD Pipeline POC 22!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
